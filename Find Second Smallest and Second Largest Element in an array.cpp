a52512ebaf930078a412afe02e03b8827177d84b
Mohammad Aazen <mohdaazen@gmail.com>
2024-08-31 17:50:17 +0530

    Create Find Second Smallest and Second Largest Element in an array.cpp

---
+++ Find Second Smallest and Second Largest Element in an array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    // Write your code here.
+    sort(a.begin(),a.end());
+    return {a[n-2],a[1]};
+}
